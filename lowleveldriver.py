b4aabac2bf4675d8d5c4160b3758bee82e3d548d
Ron Danon <rondanon@gmail.com>
2017-05-25 16:53:33 +0300

    calculate curve

--- lowleveldriver.py
+++ lowleveldriver.py
@@ -45,17 +45,21 @@ class LowLevelDriver(object):
         track_edges=np.zeros((19,2))
         counter=0
         for y,x in zip(track_y,track_x):
-            track_edges[counter]=[y,x]
+            track_edges[counter]=[x,y]
             counter+=1
         return track_edges
         
     def findGeneralDirection(self):
         leftMax =  np.argmax(self.state.track)
         rightMax =  len(self.state.track) - np.argmax(np.flip(self.state.track) -1
-        rightTrack = track_edges[rightMax+1:]
-        leftTrack = track_edges[:leftMax]
-        np.polyfit
-        
+        rightTrack = track_edges[rightMax+1:,:]
+        leftTrack = track_edges[:leftMax,:]
+        rightPoly = np.polyfit(rightTrack[:,0],rightTrack[:,1])
+        leftPoly =  np.polyfit(leftTrack[:,0],leftTrack[:,1])
+        rightCurve = ((1 + (2*rightPoly[0]*self.rightTrack[:,0] + rightPoly[1])**2)**1.5) / np.absolute(2*rightPoly[0])
+        leftCurve = ((1 + (2*leftPoly[0]*self.leftTrack[:,0] + leftPoly[1])**2)**1.5) / np.absolute(2*leftPoly[0])
+        curve = (rightCurve+leftCurve)/2
+        return curve
         
         
     
